1e49ffa4e03b773f00cc51e92c31f20e10820061
Dalton Gomez <69910162+DaltonGomez@users.noreply.github.com>
2022-07-07 15:29:32 -0600

    Update hyperparameter tuner script

--- run_hpTuner.py
+++ run_hpTuner.py
@@ -14,31 +14,43 @@ python3 run_hpTuner.py
 
 if __name__ == "__main__":
     # Input graph and tuner object w/ options
-    inputGraphs = ["medium_0", "medium_1"]
-    hpTuner = HyperparamTuner(inputGraphs, 2)
+    inputGraphs = [
+                "large_UB_0",
+                "large_UB_1",
+                "large_UB_2",
+                "large_UB_3",
+                "large_UB_4",
+                "large_UB_5",
+                "large_UB_6",
+                "large_UB_7",
+                "large_UB_8",
+                "large_UB_9"]
+    runsPerGraph = 3
+    hpTuner = HyperparamTuner(inputGraphs, runsPerGraph)
 
     # Update the HP tuning search space
     hpTuner.hpSpace = {
-        "isOneDimAlphaTable": [True, False],
-        "isOptimizedArcSelections": [True, False],
-        "populationSize": [5, 10],
-        "numGenerations": [5, 10],
-        "initializationStrategy": ["perEdge", "perArc"],
-        "initializationDistribution": ["digital"],
-        "initializationParams": [
-            [0, 100000],
-            [1, 100000]
-            ],
-        "selectionMethod": ["tournament", "roulette"],
-        "tournamentSize": [3, 5],
-        "crossoverMethod": ["onePoint", "twoPoint"],
-        "crossoverRate": [0.50, 1.0],
-        "crossoverAttemptsPerGeneration": [1, 3],
-        "replacementStrategy": ["replaceWeakestTwo", "replaceParents"],
-        "mutationMethod": ["randomPerArc", "randomPerEdge"],
-        "mutationRate": [0.10, 0.50],
-        "perArcEdgeMutationRate": [0.10, 0.50]
-    }
+                "isOneDimAlphaTable": [True],
+                "isOptimizedArcSelections": [True],
+                "populationSize": [10, 25, 50, 100],
+                "numGenerations": [10, 25, 50, 100],
+                "initializationStrategy": ["perEdge"],
+                "initializationDistribution": ["digital"],
+                "initializationParams": [
+                                            [0, 100000],
+                                            [1, 100000],
+                                            [10, 100000]
+                                        ],
+                "selectionMethod": ["tournament", "roulette", "random"],
+                "tournamentSize": [3, 5, 8],
+                "crossoverMethod": ["onePoint", "twoPoint"],
+                "crossoverRate": [0.70, 1.0],
+                "crossoverAttemptsPerGeneration": [1, 3],
+                "replacementStrategy": ["replaceWeakestTwo", "replaceParents"],
+                "mutationMethod": ["randomPerEdge", "randomTotal"],
+                "mutationRate": [0.01, 0.05, 0.10, 0.25, 0.50],
+                "perArcEdgeMutationRate": [0.01, 0.05, 0.10, 0.25, 0.50]
+                }
 
     # Solve the graph
     # NOTE - The grid search is not comprehensive of all the hyperparameters that could be tuned
