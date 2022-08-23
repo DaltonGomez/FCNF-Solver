f53242f98d0fcea5e58e83edf4320698d5f43fb0
Dalton Gomez <69910162+DaltonGomez@users.noreply.github.com>
2022-08-23 17:58:07 -0600

    Test of SSH connection to campus computer

--- sandbox.py
+++ sandbox.py
@@ -1,4 +1,3 @@
-from src.Experiments.GAvsMILP import GAvsMILP
 
 """
 RUN COMMAND:
@@ -11,45 +10,4 @@ python3 sandbox.py
 """
 
 if __name__ == "__main__":
-    # Input graph and experiment object w/ options
-    inputGraph = "small_UB_0"
-    sandboxSolver = GAvsMILP(inputGraph, isSolvedWithGeneticAlg=False, isOneDimAlphaTable=True,
-                              isOptimizedArcSelections=True, isSolvedWithMILP=True, isRace=False,
-                              isDrawing=True, isLabeling=False, isGraphing=True, isOutputtingCPLEX=True)
-
-    # Alpha-GA population attribute & hyperparameters
-    sandboxSolver.geneticPop.setPopulationHyperparams(populationSize=20,
-                                             numGenerations=40,
-                                             terminationMethod="setGenerations")
-    sandboxSolver.geneticPop.setInitializationHyperparams(initializationStrategy="perEdge",
-                                                 initializationDistribution="gaussian",
-                                                 initializationParams=[500.0, 100.0])
-    sandboxSolver.geneticPop.setIndividualSelectionHyperparams(selectionMethod="tournament",
-                                                      tournamentSize=5)
-    sandboxSolver.geneticPop.setCrossoverHyperparams(crossoverMethod="twoPoint",
-                                            crossoverRate=1.0,
-                                            crossoverAttemptsPerGeneration=1,
-                                            replacementStrategy="replaceWeakestTwo")
-    sandboxSolver.geneticPop.setMutationHyperparams(mutationMethod="randomPerEdge",
-                                           mutationRate=0.20,
-                                           mutationStrength=0.25)
-    sandboxSolver.geneticPop.setDaemonHyperparams(isDaemonUsed=True, daemonAnnealingRate=0.10,
-                                                  daemonStrategy="globalMedian", daemonStrength=0.10)
-    # Solve the graph
-    sandboxSolver.solveGraph()
-
-    """
-    # DEMOS WHY THE POST-PROCESSING ARC OPTIMIZATION MAKES THE GA SO GOOD AS IT EVEN MAKES A SUBSTANTIAL DIFFERENCE ON THE MILP
-    rawMILPArcFlows = sandboxSolver.milpCplexSolver.model.solution.get_value_dict(sandboxSolver.milpCplexSolver.arcFlowVars)
-    optMILPArcFlows = sandboxSolver.milpCplexSolver.optimizeArcSelection(sandboxSolver.milpCplexSolver.model.solution.get_value_dict(sandboxSolver.milpCplexSolver.arcFlowVars))
-
-    for arcCapTup in rawMILPArcFlows.keys():
-        if rawMILPArcFlows[arcCapTup] > 0.0:
-            print("RAW")
-            print(arcCapTup)
-            print(rawMILPArcFlows[arcCapTup])
-        if optMILPArcFlows[arcCapTup] > 0.0:
-            print("OPT")
-            print(arcCapTup)
-            print(optMILPArcFlows[arcCapTup])
-    """
+    print("Write sandbox code here...")
