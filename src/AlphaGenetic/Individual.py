718adfb5b7cdf25d65cbc7981f8a608fb5ee9c7f
Dalton Gomez <69910162+DaltonGomez@users.noreply.github.com>
2022-09-25 23:45:46 -0600

    Start the implementation of the pathlet construction methods

--- src/AlphaGenetic/Individual.py
+++ src/AlphaGenetic/Individual.py
@@ -3,6 +3,7 @@ from typing import Dict, Tuple, List
 
 from numpy import ndarray
 
+from FlowNetwork.Pathlet import Pathlet
 from src.FlowNetwork.FlowNetworkSolution import FlowNetworkSolution
 from src.Graph.CandidateGraph import CandidateGraph
 
@@ -28,6 +29,8 @@ class Individual:
         # Returned Cost (a.k.a. the fitness of the individual)
         self.trueCost: float = 0.0  # True cost computed by using the assigned flows and the Fixed Charge Network Flow model formulation
         self.fakeCost: float = 0.0  # Actual objective value returned by the solver (used only for debugging purposes)
+        # Pathlet Attributes
+        self.pathlets: List[Pathlet] = []  # Holds the set of pathlets that make up the flow network solution
 
     # ============================================
     # ============== HELPER METHODS ==============
@@ -55,10 +58,49 @@ class Individual:
     def writeIndividualAsSolution(self, minTargetFlow: float, optionalDescription="") -> FlowNetworkSolution:
         """Writes the current expressed network as a solution instance"""
         if self.isSolved is False:
-            print("You must solve the individual before writing a solution!")
+            print("ERROR - Cannot write a solution from an unsolved individual!")
         else:
             thisSolution = FlowNetworkSolution(self.graph, minTargetFlow, self.fakeCost, self.trueCost,
                                                self.srcFlows, self.sinkFlows, self.arcFlows, "Alpha_GA", False,
                                                self.graph.isSourceSinkCapacitated, self.graph.isSourceSinkCharged,
                                                optionalDescription=optionalDescription)
             return thisSolution
+
+    # =============================================
+    # ============== PATHLET METHODS ==============
+    # =============================================
+    def computeAllPathlets(self) -> None:
+        """Computes all the pathlets that have a positive flow"""
+        # Declare a stack to maintain which nodes still need to be checked
+        uncheckedNodesStack = []
+        # Iterate over all sources and push them on the stack if they have a positive flow
+        for srcIndex in range(self.graph.numSources):
+            if self.srcFlows[srcIndex] > 0:
+                nodeID = self.graph.sourcesArray[srcIndex]
+                uncheckedNodesStack.append(nodeID)
+        # Until the stack is empty, evaluate each node and build pathlets
+        while len(uncheckedNodesStack) > 0:
+            thisStartingNode = uncheckedNodesStack.pop()
+            self.evaluateStartingNode(thisStartingNode, uncheckedNodesStack)
+
+    def evaluateStartingNode(self, startingNodeID: int, uncheckedNodeStack: List[int]) -> None:
+        """Builds all possible pathlets that start at the starting node and appends them to the individual's pathlets"""
+        currentNodeObj = self.graph.nodesDict[startingNodeID]
+        for outgoingEdge in currentNodeObj.outgoingEdges:
+            edgeIndex = self.graph.edgesDict[outgoingEdge]
+            for capIndex in range(len(self.graph.possibleArcCapsArray)):
+                capacity = self.graph.possibleArcCapsArray[capIndex]
+                arcObj = self.graph.arcsDict[(outgoingEdge[0], outgoingEdge[1], capacity)]
+                if self.arcFlows[(edgeIndex, capIndex)] > 0.0:
+                    pass
+                    # TODO - Implement the build pathlet method and call here on each outgoing flow
+
+    def buildPathlet(self, startingNodeID: int, uncheckedNodeStack: List[int]) -> Pathlet:
+        """Builds the pathlet from the starting node, terminating when a source, sink or node of degree 3+ is seen"""
+        thisPathlet = Pathlet(startingNodeID)
+        return thisPathlet
+        # TODO - Finish implementing
+        # NOTE:
+        # A pathlet should terminate when a source, sink, or node of degree 3+ is seen
+        # Must be recursive to call the next pathlet construction after a pathlet terminates (via the node stack)
+        # Also, account for source starting nodes with multiple outgoing edges and sink nodes with outgoing edges
--- src/FlowNetwork/Pathlet.py
+++ src/FlowNetwork/Pathlet.py
@@ -1,19 +1,18 @@
-from typing import List
+from typing import List, Tuple
 
 
 class Pathlet:
     """Class that stores the information for a pathlet in the flow network solution to a candidate graph instance"""
 
-    def __init__(self, startingNode: int):
+    def __init__(self, startingNodeID: int):
         """Constructor of a pathlet instance"""
         # Composition attributes
-        self.startingNode: int = startingNode  # Integer ID of the node that begins the pathlet
-        self.endingNode: int = -1  # Integer ID of the node that ends the pathlet
-        self.allNodes: List = []  # List of all node IDs that make up the pathlet
+        self.nodes: List[int] = [startingNodeID]  # List of all node IDs that make up the pathlet
+        self.arcs: List[Tuple[int, int]] = []  # List of all arc indexes that make up the pathlet
         # Descriptive attributes
-        self.size: int = -1  # Number of edges in the pathlet
+        self.size: int = -1  # Number of arcs in the pathlet
         self.totalFlow: float = -1.0  # Amount of flow transported by pathlet
-        self.variableCost: float = -1.0
-        self.fixedCost: float = -1.0
+        self.variableCost: float = -1.0  # Variable cost of the pathlet
+        self.fixedCost: float = -1.0  # Fixed cost of the pathlet
         self.totalCost: float = -1.0  # Cost of the edges in the pathlet
-        self.totalCostPerFlow: float = -1.0
+        self.totalCostPerFlow: float = -1.0  # Ratio of flow transported to total cost of the pathlet
